a3bf633c9fa477f5bd1d9dfb6c5790dcd7534c86
alexexisted <a.assjones@list.ru>
2025-08-21 16:06:29 +0200

    bug fixes: - some kind of normal(not) navbar

--- app/src/main/java/polako/cloud/clotho/MainActivity.kt
+++ app/src/main/java/polako/cloud/clotho/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -31,7 +30,7 @@ class MainActivity : ComponentActivity() {
 
                 val navBackStackEntry by navController.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry?.destination?.route
-                
+
                 Scaffold(
                     bottomBar = {
                         if (currentRoute == Routes.MAIN_SCREEN || currentRoute == Routes.HISTORY_SCREEN) {
