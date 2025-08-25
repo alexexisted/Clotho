50ecd1c8d79a1527494615abaa2e63ccd531b512
alexexisted <a.assjones@list.ru>
2025-08-25 22:32:34 +0200

    features: - navigation update(я все сломала) - system navbar is hiddent now

--- app/src/main/java/polako/cloud/clotho/presentation/focus_screen/FocusScreen.kt
+++ app/src/main/java/polako/cloud/clotho/presentation/focus_screen/FocusScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import polako.cloud.clotho.navigation.Routes
 import polako.cloud.clotho.presentation.reflection_screen.ReflectionBS
 import polako.cloud.clotho.ui.composables.Stopwatch
@@ -23,46 +24,50 @@ import polako.cloud.clotho.ui.composables.Stopwatch
 @Composable
 fun FocusScreen(
     navController: NavController,
-    viewModel: FocusViewModel = hiltViewModel()
+    viewModel: FocusViewModel = hiltViewModel(),
 ) {
     val state by viewModel.uiState.collectAsState()
     val gradientColors = listOf(Color(0xFF006187), Color(0xFF313131))
 
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.linearGradient(
-                    gradientColors
-                )
-            )
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(
+                    brush =
+                        Brush.linearGradient(
+                            gradientColors,
+                        ),
+                ),
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+            verticalArrangement = Arrangement.SpaceBetween,
         ) {
             state.activity?.let { activity ->
                 Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp),
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
+                    horizontalArrangement = Arrangement.Center,
                 ) {
                     Icon(
                         painter = painterResource(id = activity.icon),
                         contentDescription = null,
                         tint = Color.White,
-                        modifier = Modifier.size(32.dp)
+                        modifier = Modifier.size(32.dp),
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = activity.name,
                         style = MaterialTheme.typography.headlineMedium,
-                        color = Color.White
+                        color = Color.White,
                     )
                 }
             }
@@ -73,7 +78,7 @@ fun FocusScreen(
                 onStop = {
                     viewModel.onAction(FocusUIAction.Stop)
                 },
-                onStart = { viewModel.onAction(FocusUIAction.Start) }
+                onStart = { viewModel.onAction(FocusUIAction.Start) },
             )
             Spacer(modifier = Modifier.height(16.dp))
         }
@@ -83,13 +88,19 @@ fun FocusScreen(
                 onDismissRequest = {
                     viewModel.onAction(FocusUIAction.DismissReflection)
                 },
-                onSaveClicked = { score, tags -> 
+                onSaveClicked = { score, tags ->
                     viewModel.onAction(FocusUIAction.SaveReflection(score, tags))
-                    navController.navigate(Routes.MAIN_SCREEN)
+                    navController.navigate(Routes.MAIN_SCREEN) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 },
                 reflectionScore = state.initialReflectionScore,
                 reflectionTags = state.reflectionChips,
-                selectedTags = state.selectedTags
+                selectedTags = state.selectedTags,
             )
         }
     }
