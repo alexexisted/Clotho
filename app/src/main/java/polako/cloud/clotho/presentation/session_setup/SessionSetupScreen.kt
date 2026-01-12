d8545d93543082a527c852d40b1e7bd42a3f29ab
alexa gabusheva <alexa.gabusheva@gmail.com>
2026-01-12 12:41:15 +0100

    redesign release

--- app/src/main/java/polako/cloud/clotho/presentation/session_setup/SessionSetupScreen.kt
+++ app/src/main/java/polako/cloud/clotho/presentation/session_setup/SessionSetupScreen.kt
@@ -34,7 +34,6 @@ import polako.cloud.clotho.ui.composables.ActivityItem
 fun SessionSetupScreen(
     navController: NavController,
     viewModel: SessionSetupViewModel = hiltViewModel(),
-    sharedViewModel: SharedFocusViewModel = hiltViewModel(),
 ) {
     val state by viewModel.uiState.collectAsState()
 
